a00b5bfe15a3860b78cbba832303da5bc80768d2
Xycone <seanang123456@gmail.com>
2023-06-23 16:41:07 +0800

    remove test

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
-// testing
+
 // Simple Route
 app.get("/", (req, res) => {
     res.send("Welcome!");
