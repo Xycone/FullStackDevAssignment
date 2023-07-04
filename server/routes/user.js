e2a9fa080ad1c0ef53c253cce07ca5ddb81cb7d9
Xycone <seanang123456@gmail.com>
2023-07-05 00:11:13 +0800

    missing Sequelize was causing errors

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-const { User } = require("../models");
+const { User, Sequelize } = require("../models");
 const yup = require("yup");
 const { sign } = require("jsonwebtoken");
 const { validateToken } = require("../middlewares/auth");
