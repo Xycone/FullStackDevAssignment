a0ea97e1c0f069b39407b0738da735f09f503f91
FIamenova <josephongyz302@gmail.com>
2023-07-06 16:12:11 +0800

    banana

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,9 +27,14 @@ import AddPayment from './pages/AddPayment';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { green } from '@mui/material/colors';
 const theme = createTheme({
-  palette: {
-    primary: green,
-  },
+    palette: {
+      primary: {
+        main: '#1b5e20',
+      },
+      secondary: {
+        main: '#66bb6a',
+      },
+    },
 });
 
 function App() {
@@ -50,78 +55,81 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Router>
-        <AppBar position="static" className='AppBar'>
-          <Container>
-            <Toolbar disableGutters={true}>
-              <Link to="/">
-                <Typography variant="h6" component="div">
-                  Learning
-                </Typography>
-              </Link>
-              <Link to="/cars" >
-                <Typography>
-                  Car Listings(Admin)
-                </Typography>
-              </Link>
-              <Link to="/payment" >
-                <Typography>
-                  Report
-                </Typography>
-              </Link>
-              <Link to="/apayment" >
-                <Typography>
-                  Payment
-                </Typography>
-              </Link>
-              <Link to="/feedback" >
-                <Typography>
-                  All feedback
-                </Typography>
-              </Link>
-              <Link to="/contactus" >
-                <Typography>
-                  Contact Us
-                </Typography>
-              </Link>
-              <Link to="/payment" >
-                <Typography>
-                  Payment
-                </Typography>
-              </Link>
-              <Box sx={{ flexGrow: 1 }}></Box>
-              {user && (
-                <>
-                  <Typography>{user.name}</Typography>
-                  <Typography>
-                    <Button style={{ color: "white", marginLeft: "20px" }} onClick={logout}>Logout</Button>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Router>
+          <AppBar position="static" className='AppBar'>
+            <Container>
+              <Toolbar disableGutters={true}>
+                <Link to="/">
+                  <Typography variant="h6" component="div">
+                    Learning
                   </Typography>
-                </>
-              )
-              }
-              {!user && (
-                <>
-                  <Link to="/login" ><Typography>Login</Typography></Link>
-                </>
-              )}
-            </Toolbar>
+                </Link>
+                <Link to="/cars" >
+                  <Typography>
+                    Car Listings(Admin)
+                  </Typography>
+                </Link>
+                <Link to="/apayment" >
+                  <Typography>
+                    Checkout
+                  </Typography>
+                </Link>
+                <Link to="/usertable" >
+                  <Typography>
+                    User Table
+                  </Typography>
+                </Link>
+                <Link to="/feedback" >
+                  <Typography>
+                    All feedback
+                  </Typography>
+                </Link>
+                <Link to="/contactus" >
+                  <Typography>
+                    Contact Us
+                  </Typography>
+                </Link>
+                <Link to="/payment" >
+                  <Typography>
+                    Report
+                  </Typography>
+                </Link>
+                <Box sx={{ flexGrow: 1 }}></Box>
+                {user && (
+                  <>
+                    <Typography>{user.name}</Typography>
+                    <Typography>
+                      <Button style={{ color: "white", marginLeft: "20px" }} onClick={logout}>Logout</Button>
+                    </Typography>
+                  </>
+                )
+                }
+                {!user && (
+                  <>
+                    <Link to="/login" ><Typography>Login</Typography></Link>
+                  </>
+                )}
+              </Toolbar>
+            </Container>
+          </AppBar>
+          <Container>
+            <Routes>
+              <Route path={"/cars"} element={<CarListings />} />
+              <Route path={"/addcars"} element={<AddCarListings />} />
+              <Route path={"/editcars/:id"} element={<EditCarListings />} />
+              <Route path={"/viewcars"} element={<ViewCarListings />} />
+              <Route path={"/login"} element={<Login />} />
+              <Route path={"/feedback"} element={<AllFeedback />} />
+              <Route path={"/contactus"} element={<ContactUs />} />
+              <Route path={"/apayment"} element={<AddPayment />} />
+              <Route path={"/payment"} element={<Payment />} />
+              <Route path={"/register"} element={<Register />} />
+              <Route path={"/usertable"} element={<UserTable />}></Route>
+            </Routes>
           </Container>
-        </AppBar>
-        <Container>
-          <Routes>
-            <Route path={"/cars"} element={<CarListings />} />
-            <Route path={"/addcars"} element={<AddCarListings />} />
-            <Route path={"/editcars/:id"} element={<EditCarListings />} />
-            <Route path={"/viewcars"} element={<ViewCarListings />} />
-            <Route path={"/login"} element={<Login />} />
-            <Route path={"/feedback"} element={<AllFeedback />} />
-            <Route path={"/contactus"} element={<ContactUs />} />
-            <Route path={"/apayment"} element={<AddPayment />} />
-            <Route path={"/payment"} element={<Payment />} />
-            <Route path={"/register"} element={<Register />} />
-          </Routes>
-        </Container>
-      </Router>
+        </Router>
+      </UserContext.Provider>
     </ThemeProvider>
   );
 }
