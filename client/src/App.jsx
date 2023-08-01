7d3f72d2fbcabdd1daadd154e876a8a055650545
Xycone <seanang123456@gmail.com>
2023-08-01 20:38:39 +0800

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import http from './http';
 import { Container, AppBar, Toolbar, Typography, Box, Button } from '@mui/material';
 import { BrowserRouter as Router, Routes, Route, Link }
   from 'react-router-dom';
+import CssBaseline from "@mui/material/CssBaseline";
 import UserContext from './contexts/UserContext';
 
 import Discounts from './pages/Discounts';
@@ -33,12 +34,14 @@ import {
   Experimental_CssVarsProvider as MaterialCssVarsProvider,
   THEME_ID as MATERIAL_THEME_ID,
 } from '@mui/material/styles';
-import { Create } from '@mui/icons-material';
 
 const materialTheme = materialExtendTheme({
   colorSchemes: {
     light: {
       palette: {
+        background: {
+          default: '#F5F5F5',
+        },
         primary: {
           main: '#1b5e20',
         },
@@ -69,102 +72,104 @@ function App() {
 
   return (
     <MaterialCssVarsProvider theme={{ [MATERIAL_THEME_ID]: materialTheme }}>
-      <UserContext.Provider value={{ user, setUser }}>
-        <Router>
-          <AppBar position="static" className='AppBar'>
-            <Container>
-              <Toolbar disableGutters={true}>
-                <Link to="/">
-                  <Typography variant="h6" component="div">
-                    Learning
-                  </Typography>
-                </Link>
-                <Link to="/listings" >
-                  <Typography>
-                    Car Listings(Admin)
-                  </Typography>
-                </Link>
-                <Link to="/viewlistings" >
-                  <Typography>
-                    Car Listings(User)
-                  </Typography>
-                </Link>
-                <Link to="/cars" >
-                  <Typography>
-                    Car Item(Admin)
-                  </Typography>
-                </Link>
-                <Link to="/apayment" >
-                  <Typography>
-                    Checkout
-                  </Typography>
-                </Link>
-                <Link to="/usertable" >
-                  <Typography>
-                    User Table
-                  </Typography>
-                </Link>
-                <Link to="/feedback" >
-                  <Typography>
-                    All feedback
-                  </Typography>
-                </Link>
-                <Link to="/discounts" >
-                  <Typography>
-                    Discount
-                  </Typography>
-                </Link>
-                <Link to="/adddiscount" >
-                  <Typography>
-                    Add Discount
-                  </Typography>
-                </Link>
-                <Box sx={{ flexGrow: 1 }}></Box>
-                {user && (
-                  <>
-                    <Typography>{user.name}</Typography>
-                    <Typography>
-                      <Button style={{ color: "white", marginLeft: "20px" }} onClick={logout}>Logout</Button>
+      <CssBaseline>
+        <UserContext.Provider value={{ user, setUser }}>
+          <Router>
+            <AppBar position="static" className='AppBar'>
+              <Container>
+                <Toolbar disableGutters={true}>
+                  <Link to="/">
+                    <Typography variant="h6" component="div">
+                      Learning
                     </Typography>
-                  </>
-                )
-                }
-                {!user && (
-                  <>
-                    <Link to="/login" ><Typography>Login</Typography></Link>
-                  </>
-                )}
-              </Toolbar>
-            </Container>
-          </AppBar>
-          <Container>
-            <Routes>
-              <Route path={"/listings"} element={<CarListings />} />
-              <Route path={"/addlistings"} element={<AddCarListings />} />
-              <Route path={"/editlistings/:id"} element={<EditCarListings />} />
-              <Route path={"/viewlistings"} element={<ViewCarListings />} />
-              <Route path={"/cars"} element={<CarItem />} />
-              <Route path={"/addcars"} element={<AddCarItem />} />
-              <Route path={"/editcars/:id"} element={<EditCarItem />} />
-              <Route path={"/createbooking/:id"} element={<CreateBooking />} />
-              <Route path={"/login"} element={<Login />} />
-              <Route path={"/feedback"} element={<AllFeedback />} />
-              <Route path={"/contactus"} element={<ContactUs />} />
-              <Route path={"/apayment"} element={<AddPayment />} />
-              <Route path={"/payment"} element={<Payment />} />
-              <Route path={"/register"} element={<Register />} />
-              <Route path={"/usertable"} element={<UserTable />}></Route>
-              <Route path={"/updateuser/:id"} element={<UpdateUser />}></Route>
-              <Route path={"/discounts"} element={<Discounts />} />
-              <Route path={"/editdiscount/:id"} element={<EditDiscount />} />
-              <Route path={"/adddiscount"} element={<AddDiscount />} />
-              <Route path={"/viewdiscount"} element={<ViewDiscounts />} />
-              <Route path={"/paymentpage"} element={<PaymentPage />} />
+                  </Link>
+                  <Link to="/listings" >
+                    <Typography>
+                      Car Listings(Admin)
+                    </Typography>
+                  </Link>
+                  <Link to="/viewlistings" >
+                    <Typography>
+                      Car Listings(User)
+                    </Typography>
+                  </Link>
+                  <Link to="/cars" >
+                    <Typography>
+                      Car Item(Admin)
+                    </Typography>
+                  </Link>
+                  <Link to="/apayment" >
+                    <Typography>
+                      Checkout
+                    </Typography>
+                  </Link>
+                  <Link to="/usertable" >
+                    <Typography>
+                      User Table
+                    </Typography>
+                  </Link>
+                  <Link to="/feedback" >
+                    <Typography>
+                      All feedback
+                    </Typography>
+                  </Link>
+                  <Link to="/discounts" >
+                    <Typography>
+                      Discount
+                    </Typography>
+                  </Link>
+                  <Link to="/adddiscount" >
+                    <Typography>
+                      Add Discount
+                    </Typography>
+                  </Link>
+                  <Box sx={{ flexGrow: 1 }}></Box>
+                  {user && (
+                    <>
+                      <Typography>{user.name}</Typography>
+                      <Typography>
+                        <Button style={{ color: "white", marginLeft: "20px" }} onClick={logout}>Logout</Button>
+                      </Typography>
+                    </>
+                  )
+                  }
+                  {!user && (
+                    <>
+                      <Link to="/login" ><Typography>Login</Typography></Link>
+                    </>
+                  )}
+                </Toolbar>
+              </Container>
+            </AppBar>
+            <Container>
+              <Routes>
+                <Route path={"/listings"} element={<CarListings />} />
+                <Route path={"/addlistings"} element={<AddCarListings />} />
+                <Route path={"/editlistings/:id"} element={<EditCarListings />} />
+                <Route path={"/viewlistings"} element={<ViewCarListings />} />
+                <Route path={"/cars"} element={<CarItem />} />
+                <Route path={"/addcars"} element={<AddCarItem />} />
+                <Route path={"/editcars/:id"} element={<EditCarItem />} />
+                <Route path={"/createbooking/:id"} element={<CreateBooking />} />
+                <Route path={"/login"} element={<Login />} />
+                <Route path={"/feedback"} element={<AllFeedback />} />
+                <Route path={"/contactus"} element={<ContactUs />} />
+                <Route path={"/apayment"} element={<AddPayment />} />
+                <Route path={"/payment"} element={<Payment />} />
+                <Route path={"/register"} element={<Register />} />
+                <Route path={"/usertable"} element={<UserTable />}></Route>
+                <Route path={"/updateuser/:id"} element={<UpdateUser />}></Route>
+                <Route path={"/discounts"} element={<Discounts />} />
+                <Route path={"/editdiscount/:id"} element={<EditDiscount />} />
+                <Route path={"/adddiscount"} element={<AddDiscount />} />
+                <Route path={"/viewdiscount"} element={<ViewDiscounts />} />
+                <Route path={"/paymentpage"} element={<PaymentPage />} />
 
-            </Routes>
-          </Container>
-        </Router>
-      </UserContext.Provider>
+              </Routes>
+            </Container>
+          </Router>
+        </UserContext.Provider>
+      </CssBaseline>
     </MaterialCssVarsProvider>
   );
 }
